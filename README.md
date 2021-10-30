0df140fef2aca7f20391d54db19518bc09ed57f4
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2021-10-30 16:51:29 +0500

    fix: initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Add-ons
+
+[![Travis](https://img.shields.io/travis/nanoexpress/addons.svg)](https://github.com/nanoexpress/addons)
+[![Code Climate](https://codeclimate.com/github/nanoexpress/addons/badges/gpa.svg)](https://codeclimate.com/github/nanoexpress/addons)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/nanoexpress/addons/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/nanoexpress/addons/?branch=master)
+[![GitHub issues](https://img.shields.io/github/issues/nanoexpress/addons.svg)](https://github.com/nanoexpress/addons/issues)
+
+List of addons for nanoexpress
+
+## Features
+
+- Better documentation
+- Has own examples
+- Cleaner structure
+- TypeScript/IDE Support
+- JSDoc Annonations
+
+## List
+
+For more info, go-to add-on documentation by click to link
+
+- [swagger-parse](./packages/swagger-parse)
+
+## License
+
+MIT
---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import { dependencies } from './package.json';
+
+const external = Object.keys(dependencies).concat([
+  'events',
+  'http',
+  'zlib',
+  'fs',
+  'fs/promises'
+]);
+
+export default ['swagger-parse'].map((name) => ({
+  input: `./packages/${name}/src/${name}.ts`,
+  output: [
+    {
+      format: 'esm',
+      file: `./packages/${name}/esm/${name}.esm.js`,
+      strict: true,
+      sourcemap: true,
+      exports: 'auto'
+    },
+    {
+      format: 'cjs',
+      file: `./packages/${name}/cjs/${name}.cjs.js`,
+      strict: true,
+      sourcemap: true,
+      exports: 'auto'
+    }
+  ],
+  plugins: [
+    resolve(),
+    typescript({
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      rollupCommonJSResolveHack: false,
+      clean: true,
+      useTsconfigDeclarationDir: true
+    })
+  ],
+  external
+}));
